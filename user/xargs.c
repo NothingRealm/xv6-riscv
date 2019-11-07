4da41afde0ea94ccbd398dbdc41880dcaf78e1fb
nothingrealm <shafaghi1378@gmail.com>
2019-11-07 13:35:52 +0330

    need a little more job

--- user/xargs.c
+++ user/xargs.c
@@ -1,42 +1,40 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include <stddef.h>
+
 
 #define MAX 100
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	char buf[MAX] = " ";
-	//printf("%d	%s	%s \n", argc, argv[1], argv[2]);
 	
-	while(strcmp(buf, "")) {
+	while(strcmp(buf, "")) 
+	{
 		gets(buf, MAX);
-		if (strcmp(buf, "")) {
+		if (strcmp(buf, ""))
+		{
 			int pid = fork();
-			if (pid == 0) {
-			int x = strlen(buf);
-			buf[x - 1] = '\0';
-			char *arg[argc] ;
-			for (int i = 0; i < argc - 1; i++) {
-				arg[i] = argv[i + 1];
-//				printf("%s\n", arg[i]);
+			if (pid == 0)
+			{
+				int x = strlen(buf);
+				buf[x - 1] = '\0';
+				char *arg[argc] ;
+				for (int i = 0; i < argc - 1; i++)
+				{
+					arg[i] = argv[i + 1];
+				}
+				arg[argc - 1] = buf;
+				arg[argc] = NULL;
+				exec(arg[0], arg);
+				exit(0);
 			}
-			arg[argc - 1] = buf;
-			arg[argc] = "";
-//			printf("%s\n", arg[argc]);
-//			for (int i = 0; i < argc; i++) {
-//				printf("%s\n", arg[i]);
-//			}
-			argv[argc - 1] = buf;
-			exec(argv[1], arg);
-			exit(0);
-			}
-		 else if(pid > 0) {
+		 else if(pid > 0)
+		 {
 			wait(0);
-			}
+		 }
 		}	
 	}
-
-
 	exit(0);
-
 }
